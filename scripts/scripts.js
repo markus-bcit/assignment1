39ad3d57ebe8ea3b3b7b8fd6740abb2dd2be22d5
Markus Afonso <mafonso4@my.bcit.ca>
2022-11-28 19:34:29 -0800

    adds note title from array to aside

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,51 +1,57 @@
 const aside = document.querySelector('aside')
 const a = document.querySelector('a')
-const button1 = document.querySelector('.button1')
-const button2 = document.querySelector('.button2')
-const button3 = document.querySelector('.button3')
-const button4 = document.querySelector('.button4')
+const button_new_note = document.querySelector('.button_new_note')
+const button_dark_theme = document.querySelector('.button_dark_theme')
+const button_save = document.querySelector('.button_save')
+const button_cancel = document.querySelector('.button_cancel')
 const textarea = document.querySelector('textArea')
+const notesList = document.querySelector('ul')
 
 
 function togglemode(){
     document.body.classList.toggle('bodydark')
     aside.classList.toggle('asidedark') 
-    button1.classList.toggle('buttonDB')
-    button2.classList.toggle('buttonDP')
-    button3.classList.toggle('buttonDB')
-    button4.classList.toggle('buttonDR')
+    button_new_note.classList.toggle('buttonDB')
+    button_dark_theme.classList.toggle('buttonDP')
+    button_save.classList.toggle('buttonDB')
+    button_cancel.classList.toggle('buttonDR')
     textarea.classList.toggle('textareaDM')
 }
 
-button2.addEventListener('click', togglemode)
+button_dark_theme.addEventListener('click', togglemode)
 
 function Ehidden(){
-    button3.classList.toggle('hidden')
-    button4.classList.toggle('hidden')
+    button_save.classList.toggle('hidden')
+    button_cancel.classList.toggle('hidden')
     textarea.classList.toggle('hidden')
 }
 function newnote(){
-    button3.classList.remove('hidden')
-    button4.classList.remove('hidden')
+    button_save.classList.remove('hidden')
+    button_cancel.classList.remove('hidden')
     textarea.classList.remove('hidden')
     textarea.value = null
 }
-button4.addEventListener('click', Ehidden)
-button1.addEventListener('click', newnote)
+button_cancel.addEventListener('click', Ehidden)
+button_new_note.addEventListener('click', newnote)
 
 
-let notes = [{title:"note one", body:"this is my first note"}]
+let notesArray = [{title:"note one", body:"this is my first note"}]
 
 
 let input;
 
 function savenote(){
     input = prompt("Enter Title of Note:");
-    notes.push({title: input, body: textarea.value})
+    notesArray.push({title: input, body: textarea.value})
     textarea.value = null
+    for (let i = 1; i < notesArray.length; i++) {
+        listElement = document.createElement('li')
+        notesList.appendChild(listElement)
+        listElement.textContent = notesArray[i]['title']
+    }
 }
 
-button3.addEventListener('click', savenote)
+button_save.addEventListener('click', savenote)
 
 
 // for (let x = 0; x < courselist.length; x++) {
